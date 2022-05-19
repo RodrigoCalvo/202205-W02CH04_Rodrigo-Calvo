4bebb1afeb8ab893a1a0541d7f3c293a73a0c530
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-05-19 17:02:33 +0200

    Initial commit

---
+++ arrayMethods.js
@@ -0,0 +1,246 @@
+//length, push, pop, unshift, shift, some, every, find, filter, map, findIndex, includes, indexOf, reduce, join
+export function isArray(item) {
+    if (typeof item === 'object') {
+        try {
+            if (typeof item.length === 'number') {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (e) {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+export function isFunction(myFunction) {
+    if (typeof myFunction === 'function') {
+        return true;
+    } else {
+        return false;
+    }
+}
+export function arrayLength(array) {
+    if (isArray(array)) {
+        return array.length;
+    } else {
+        throw Error();
+    }
+}
+export function push(array, item) {
+    if (isArray(array)) {
+        array[arrayLength(array)] = item;
+        return arrayLength(array);
+    } else {
+        throw new Error('Not array');
+    }
+}
+export function pop(array) {
+    if (isArray(array)) {
+        if (array.length === 0) {
+            return undefined;
+        }
+        const item = array[arrayLength(array) - 1];
+        array.length = arrayLength(array) - 1;
+        return item;
+    } else {
+        throw new Error('Not array');
+    }
+}
+export function unshift(array, item) {
+    if (isArray(array)) {
+        const aux = [item];
+        for (let i = 0; i < array.length; i++) {
+            push(aux, array[i]);
+        }
+        for (let i = 0; i < aux.length; i++) {
+            array[i] = aux[i];
+        }
+        return arrayLength(array);
+    } else {
+        throw new Error('Not array');
+    }
+}
+export function shift(array) {
+    if (isArray(array)) {
+        if (array.length === 0) {
+            return undefined;
+        }
+        const item = array[0];
+        const aux = [];
+        for (let i = 1; i < array.length; i++) {
+            push(aux, array[i]);
+        }
+        array.length = array.length - 1;
+        for (let j = 0; j < aux.length; j++) {
+            array[j] = aux[j];
+        }
+        return item;
+    } else {
+        throw new Error('Not array');
+    }
+}
+export function some(array, myFunction) {
+    if (isArray(array)) {
+        if (isFunction(myFunction)) {
+            for (let i = 0; i < array.length; i++) {
+                if (myFunction(array[i])) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            throw new Error('Not function');
+        }
+    } else {
+        throw new Error('Not array');
+    }
+}
+export function every(array, myFunction) {
+    if (isArray(array)) {
+        if (isFunction(myFunction)) {
+            for (let i = 0; i < array.length; i++) {
+                if (!myFunction(array[i])) {
+                    console.log('fallo');
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            throw new Error('Not function');
+        }
+    } else {
+        throw new Error('Not array');
+    }
+}
+export function find(array, myFunction) {
+    if (isArray(array)) {
+        if (isFunction(myFunction)) {
+            for (let i = 0; i < array.length; i++) {
+                if (myFunction(array[i])) {
+                    return array[i];
+                }
+            }
+            return undefined;
+        } else {
+            throw new Error('Not function');
+        }
+    } else {
+        throw new Error('Not array');
+    }
+}
+export function filter(array, myFunction) {
+    if (isArray(array)) {
+        if (isFunction(myFunction)) {
+            const aux = [];
+            for (let i = 0; i < array.length; i++) {
+                if (myFunction(array[i])) {
+                    push(aux, array[i]);
+                }
+            }
+            return aux;
+        } else {
+            throw new Error('Not function');
+        }
+    } else {
+        throw new Error('Not array');
+    }
+}
+export function map(array, myFunction) {
+    if (isArray(array)) {
+        if (isFunction(myFunction)) {
+            const aux = [];
+            for (let i = 0; i < array.length; i++) {
+                push(aux, myFunction(array[i]));
+            }
+            return aux;
+        } else {
+            throw new Error('Not function');
+        }
+    } else {
+        throw new Error('Not array');
+    }
+}
+export function findIndex(array, myFunction) {
+    if (isArray(array)) {
+        if (isFunction(myFunction)) {
+            for (let i = 0; i < array.length; i++) {
+                if (myFunction(array[i])) {
+                    return i;
+                }
+            }
+            return -1;
+        } else {
+            throw new Error('Not function');
+        }
+    } else {
+        throw new Error('Not array');
+    }
+}
+export function includes(array, value) {
+    if (isArray(array)) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === value) {
+                return true;
+            }
+        }
+        return false;
+    } else {
+        throw new Error('Not array');
+    }
+}
+export function indexOf(array, item) {
+    if (isArray(array)) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === item) {
+                return i;
+            }
+        }
+        return -1;
+    } else {
+        throw new Error('Not array');
+    }
+}
+export function reduce(array, myFunction, initialValue) {
+    if (isArray(array)) {
+        if (isFunction(myFunction)) {
+            let initialVal, currentValue, currentIndex;
+            if (initialValue === undefined) {
+                initialVal = array[0];
+                currentValue = array[1];
+                currentIndex = 1;
+            } else {
+                initialVal = initialValue;
+                currentValue = array[0];
+                currentIndex = 0;
+            }
+            let result = myFunction(initialVal, currentValue);
+            for (currentIndex; currentIndex < array.length - 1; ) {
+                currentValue = array[++currentIndex];
+                result = myFunction(result, currentValue);
+            }
+            return result;
+        } else {
+            throw new Error('Not function');
+        }
+    } else {
+        throw new Error('Not array');
+    }
+}
+export function join(array, separator) {
+    if (isArray(array)) {
+        const mySeparator = separator || ',';
+        let result = '';
+        for (let i = 0; i < array.length; i++) {
+            if (i === array.length - 1) {
+                result = result + array[i];
+            } else {
+                result = result + array[i] + mySeparator;
+            }
+        }
+        return result;
+    } else {
+        throw new Error('Not array');
+    }
+}
---
+++ arrayMethods.test.js
@@ -0,0 +1,492 @@
+import {
+    isArray,
+    arrayLength,
+    push,
+    pop,
+    unshift,
+    shift,
+    some,
+    every,
+    find,
+    filter,
+    map,
+    isFunction,
+    findIndex,
+    includes,
+    indexOf,
+    reduce,
+    join,
+} from './arrayMethods.js';
+
+const falseArrays = [
+    '',
+    'pepe',
+    1,
+    234252346n,
+    false,
+    undefined,
+    true,
+    null,
+    () => {},
+    { name: 'Pepe' },
+    { name: 'Pepe', length: 'Km' },
+];
+const falseFunctions = [
+    '',
+    'pepe',
+    1,
+    234252346n,
+    false,
+    undefined,
+    true,
+    null,
+    [],
+    [1, 2, 3],
+    { name: 'Pepe' },
+    { name: 'Pepe', length: 'Km' },
+];
+
+describe('Given isArray function', () => {
+    describe('When falseArrays given', () => {
+        for (let i = 0; i < falseArrays.length; i++) {
+            test('Then it should return false', () => {
+                expect(isArray(falseArrays[i])).toBe(false);
+            });
+        }
+    });
+    describe('When true arrays given', () => {
+        test('Then it should return true', () => {
+            expect(isArray([])).toBe(true);
+            expect(isArray([1, 2, 3, 'zingo']));
+        });
+    });
+});
+describe('Given isFunction function', () => {
+    describe('When falseFunctions given', () => {
+        for (let i = 0; i < falseFunctions.length; i++) {
+            test('Then it should return false', () => {
+                expect(isFunction(falseFunctions[i])).toBe(false);
+            });
+        }
+    });
+    describe('When true function given', () => {
+        test('Then it should return true', () => {
+            expect(isFunction(() => {})).toBe(true);
+        });
+    });
+});
+describe('Given arrayLength function', () => {
+    describe('When receive []', () => {
+        test('Then should return 0', () => {
+            const param = [];
+            const result = 0;
+            expect(arrayLength(param)).toBe(result);
+        });
+    });
+    describe('When receive [1, 2]', () => {
+        test('Then should return 2', () => {
+            const param = [1, 2];
+            const result = 2;
+            expect(arrayLength(param)).toBe(result);
+        });
+    });
+    describe('When receive falseArrays', () => {
+        for (let i = 0; i < falseArrays.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => arrayLength(falseArrays[i])).toThrow();
+            });
+        }
+    });
+});
+describe('Given push function', () => {
+    describe('When receive [], "zingo"', () => {
+        test('Then should return 0', () => {
+            const param1 = [];
+            const param2 = 'zingo';
+            const result = 1;
+            expect(push(param1, param2)).toBe(result);
+        });
+    });
+    describe('When receive [1, 2], "zingo"', () => {
+        test('Then the array should contain "zingo"', () => {
+            const param1 = [1, 2];
+            const param2 = 'zingo';
+            push(param1, param2);
+            expect(param1).toContain(param2);
+        });
+    });
+    describe('When receive falseArrays', () => {
+        for (let i = 0; i < falseArrays.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => push(falseArrays[i], 0)).toThrow();
+            });
+        }
+    });
+});
+describe('Given pop function', () => {
+    describe('When receive [1, 2, 3]', () => {
+        const param = [1, 2, 3];
+        test('Then should return 3', () => {
+            expect(pop(param)).toBe(3);
+        });
+        test('Then arrayLenth should return 2', () => {
+            expect(arrayLength(param)).toBe(2);
+        });
+    });
+    describe('When receive []', () => {
+        test('Then it should return undefined', () => {
+            const param = [];
+            expect(pop(param)).toBe(undefined);
+        });
+    });
+    describe('When receive falseArrays', () => {
+        for (let i = 0; i < falseArrays.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => pop(falseArrays[i])).toThrow();
+            });
+        }
+    });
+});
+describe('Given unshift function', () => {
+    describe('When receive [2, 3, 4], 1', () => {
+        test('Then should return 4', () => {
+            const param1 = [2, 3, 4];
+            const param2 = 1;
+            const expectedResult = 4;
+            expect(unshift(param1, param2)).toBe(expectedResult);
+        });
+        test('Then the array should contain 1', () => {
+            const param1 = [2, 3, 4];
+            const param2 = 1;
+            unshift(param1, param2);
+            expect(param1).toContain(param2);
+        });
+    });
+    describe('When receive falseArrays', () => {
+        for (let i = 0; i < falseArrays.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => unshift(falseArrays[i], 0)).toThrow();
+            });
+        }
+    });
+});
+describe('Given shift function', () => {
+    describe('When receive [7, 8, 9]', () => {
+        test('Then it should return 7', () => {
+            const param1 = [7, 8, 9];
+            expect(shift(param1)).toBe(7);
+        });
+        test('Then arrayLength should be 2', () => {
+            const param1 = [7, 8, 9];
+            const expectedResult = 2;
+            shift(param1);
+            expect(param1.length).toBe(expectedResult);
+        });
+    });
+    describe('When receive []', () => {
+        test('Then it should return undefined', () => {
+            const param = [];
+            expect(shift(param)).toBe(undefined);
+        });
+    });
+    describe('When receive falseArrays', () => {
+        for (let i = 0; i < falseArrays.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => shift(falseArrays[i])).toThrow();
+            });
+        }
+    });
+});
+describe('Given some function', () => {
+    describe('When receive [2, 4, 6, 8, 6, 4, 2], (i) => i % 2 === 1', () => {
+        test('Then it should return false', () => {
+            const param1 = [2, 4, 6, 8, 6, 4, 2];
+            const param2 = (i) => i % 2 === 1;
+            const expectedResult = false;
+            expect(some(param1, param2)).toBe(expectedResult);
+        });
+    });
+    describe('When receive [2, 4, 6, 8, 6, 4, 2], (i) => i % 2 === 0', () => {
+        test('Then it should return false', () => {
+            const param1 = [2, 4, 6, 8, 6, 4, 2];
+            const param2 = (i) => i % 2 === 0;
+            const expectedResult = true;
+            expect(some(param1, param2)).toBe(expectedResult);
+        });
+    });
+    describe('When receive falseArrays', () => {
+        for (let i = 0; i < falseArrays.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => some(falseArrays[i], () => {})).toThrow();
+            });
+        }
+    });
+    describe('When receive falseFunctions', () => {
+        for (let i = 0; i < falseFunctions.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => some([], falseFunctions[i])).toThrow();
+            });
+        }
+    });
+});
+describe('Given every function', () => {
+    describe('When receive [2, 4, 6, 8, 6, 4, 2], (i) => i % 2 === 0', () => {
+        test('Then it should return true', () => {
+            const param1 = [2, 4, 6, 8, 6, 4, 2];
+            const param2 = (i) => i % 2 === 0;
+            const expectedResult = true;
+            expect(every(param1, param2)).toBe(expectedResult);
+        });
+    });
+    describe('When receive [2, 4, 6, 8, 7, 4, 2], (i) => i % 2 === 0', () => {
+        test('Then it should return false', () => {
+            const param1 = [2, 4, 6, 8, 7, 4, 2];
+            const param2 = (i) => i % 2 === 0;
+            const expectedResult = false;
+            expect(every(param1, param2)).toBe(expectedResult);
+        });
+    });
+    describe('When receive falseArrays', () => {
+        for (let i = 0; i < falseArrays.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => every(falseArrays[i], () => {})).toThrow();
+            });
+        }
+    });
+    describe('When receive falseFunctions', () => {
+        for (let i = 0; i < falseFunctions.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => every([], falseFunctions[i])).toThrow();
+            });
+        }
+    });
+});
+describe('Given find function', () => {
+    describe('When receive [2, 3, 4, 5], (i) => i < 7', () => {
+        test('Then it should return 2', () => {
+            const param1 = [2, 3, 4, 5];
+            const param2 = (i) => i < 7;
+            const expectedResult = 2;
+            expect(find(param1, param2)).toBe(expectedResult);
+        });
+    });
+    describe('When receive [2, 3, 4, 5], (i) => i > 7', () => {
+        test('Then it should return undefined', () => {
+            const param1 = [2, 3, 4, 5];
+            const param2 = (i) => i > 7;
+            const expectedResult = undefined;
+            expect(find(param1, param2)).toBe(expectedResult);
+        });
+    });
+    describe('When receive falseArrays', () => {
+        for (let i = 0; i < falseArrays.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => find(falseArrays[i], () => {})).toThrow();
+            });
+        }
+    });
+    describe('When receive falseFunctions', () => {
+        for (let i = 0; i < falseFunctions.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => find([], falseFunctions[i])).toThrow();
+            });
+        }
+    });
+});
+describe('Given filter function', () => {
+    describe('When receive [1, 2, 3, 4, 5, 6], (i) => i % 2 ===0', () => {
+        test('Then it should return [2, 4, 6]', () => {
+            const param1 = [1, 2, 3, 4, 5, 6];
+            const param2 = (i) => i % 2 === 0;
+            const result = filter(param1, param2);
+            expect(result).toContain(2);
+            expect(result).toContain(4);
+            expect(result).toContain(6);
+        });
+    });
+    describe('When receive falseArrays', () => {
+        for (let i = 0; i < falseArrays.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => filter(falseArrays[i], () => {})).toThrow();
+            });
+        }
+    });
+    describe('When receive falseFunctions', () => {
+        for (let i = 0; i < falseFunctions.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => filter([], falseFunctions[i])).toThrow();
+            });
+        }
+    });
+});
+describe('Given map function', () => {
+    describe('When receive [1, 2, 3], (i) => i * 3', () => {
+        test('Then it should return [3, 6, 9]', () => {
+            const param1 = [1, 2, 3];
+            const param2 = (i) => i * 3;
+            const result = map(param1, param2);
+            expect(result).toContain(3);
+            expect(result).toContain(6);
+            expect(result).toContain(9);
+        });
+    });
+    describe('When receive falseArrays', () => {
+        for (let i = 0; i < falseArrays.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => map(falseArrays[i], () => {})).toThrow();
+            });
+        }
+    });
+    describe('When receive falseFunctions', () => {
+        for (let i = 0; i < falseFunctions.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => map([], falseFunctions[i])).toThrow();
+            });
+        }
+    });
+});
+describe('Given findIndex function', () => {
+    describe('When receive [1, 2, 3, 4], (i) => i % 2 === 0', () => {
+        test('Then it should return 2', () => {
+            const param1 = [1, 5, 3, 4];
+            const param2 = (i) => i % 2 === 0;
+            const result = findIndex(param1, param2);
+            const expectedResult = 3;
+            expect(result).toBe(expectedResult);
+        });
+    });
+    describe('When receive [1, 2, 3, 4], (i) => i > 10', () => {
+        test('Then it should return 2', () => {
+            const param1 = [1, 2, 3, 4];
+            const param2 = (i) => i > 10;
+            const result = findIndex(param1, param2);
+            const expectedResult = -1;
+            expect(result).toBe(expectedResult);
+        });
+    });
+    describe('When receive falseArrays', () => {
+        for (let i = 0; i < falseArrays.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => findIndex(falseArrays[i], () => {})).toThrow();
+            });
+        }
+    });
+    describe('When receive falseFunctions', () => {
+        for (let i = 0; i < falseFunctions.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => findIndex([], falseFunctions[i])).toThrow();
+            });
+        }
+    });
+});
+describe('Given includes function', () => {
+    describe('When receive [1, 2, 3], 2', () => {
+        test('Then should return true', () => {
+            const param1 = [1, 2, 3];
+            const param2 = 2;
+            const expectedResult = true;
+            expect(includes(param1, param2)).toBe(expectedResult);
+        });
+    });
+    describe('When receive [1, 2, 3], 4', () => {
+        test('Then should return false', () => {
+            const param1 = [1, 2, 3];
+            const param2 = 4;
+            const expectedResult = false;
+            expect(includes(param1, param2)).toBe(expectedResult);
+        });
+    });
+    describe('When receive falseArrays', () => {
+        for (let i = 0; i < falseArrays.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => includes(falseArrays[i])).toThrow();
+            });
+        }
+    });
+});
+describe('Given indexOf function', () => {
+    describe('When receive [1, 2, 3], 2', () => {
+        test('Then should return true', () => {
+            const param1 = [1, 2, 3];
+            const param2 = 2;
+            const expectedResult = 1;
+            expect(indexOf(param1, param2)).toBe(expectedResult);
+        });
+    });
+    describe('When receive [1, 2, 3], 4', () => {
+        test('Then should return false', () => {
+            const param1 = [1, 2, 3];
+            const param2 = 4;
+            const expectedResult = -1;
+            expect(indexOf(param1, param2)).toBe(expectedResult);
+        });
+    });
+    describe('When receive falseArrays', () => {
+        for (let i = 0; i < falseArrays.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => indexOf(falseArrays[i])).toThrow();
+            });
+        }
+    });
+});
+describe('Given reduce function', () => {
+    describe('When receive [1, 2, 3, 4], (previousValue, currentValue) => previousValue + currentValue', () => {
+        test('Then it should return 2', () => {
+            const param1 = [1, 2, 3, 4];
+            const param2 = (previousValue, currentValue) =>
+                previousValue + currentValue;
+            const result = reduce(param1, param2);
+            const expectedResult = 10;
+            expect(result).toBe(expectedResult);
+        });
+    });
+    describe('When receive [1, 2, 3, 4], (previousValue, currentValue) => previousValue + currentValue, 10', () => {
+        test('Then it should return 2', () => {
+            const param1 = [1, 2, 3, 4];
+            const param2 = (previousValue, currentValue) =>
+                previousValue + currentValue;
+            const param3 = 10;
+            const result = reduce(param1, param2, param3);
+            const expectedResult = 20;
+            expect(result).toBe(expectedResult);
+        });
+    });
+    describe('When receive falseArrays', () => {
+        for (let i = 0; i < falseArrays.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => reduce(falseArrays[i], () => {})).toThrow();
+            });
+        }
+    });
+    describe('When receive falseFunctions', () => {
+        for (let i = 0; i < falseFunctions.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => reduce([], falseFunctions[i])).toThrow();
+            });
+        }
+    });
+});
+describe('Given join function', () => {
+    describe('When receive [1, 2, 3], " - "', () => {
+        test('Then should return true', () => {
+            const param1 = [1, 2, 3];
+            const param2 = ' - ';
+            const expectedResult = '1 - 2 - 3';
+            expect(join(param1, param2)).toBe(expectedResult);
+        });
+    });
+    describe('When receive [1, 2, 3]', () => {
+        test('Then should return false', () => {
+            const param1 = [1, 2, 3];
+            const expectedResult = '1,2,3';
+            expect(join(param1)).toBe(expectedResult);
+        });
+    });
+    describe('When receive falseArrays', () => {
+        for (let i = 0; i < falseArrays.length; i++) {
+            test('Then it should throw Error', () => {
+                expect(() => join(falseArrays[i])).toThrow();
+            });
+        }
+    });
+});
